b4c156a78fc3eb6ad1fdbfebd6fc6ce2c7b86888
jeswr <jesse.wright@anu.edu.au>
2021-01-23 11:03:25 +1100

    WIP

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,2 @@
 // See https://github.com/schimatos/schimatos.org/blob/952980bb79be731d8cd56bb0110f86d57350424b/source/src/client/effects/validator-effects/make-validators.js for core logic to use
-/**
- * @todo this index file
- */
 export default {};
